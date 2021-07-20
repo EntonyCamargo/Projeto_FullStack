184965303e1d8efb6f5e3aa920bf4b924eda016f
entony <entonycamargo5@gmail.com>
2021-07-20 18:06:34 -0300

    20/07/2021

---
+++ ONRH/js/home_user.js
@@ -0,0 +1,15 @@
+function inscricao_curso(){
+    window.location.href=""
+}
+
+function inscricao_curso(){
+    window.location.href=""
+}
+
+function Minhas_informaçoes(){
+    window.location.href="meu_perfil_user.html"
+}
+
+function Folha_ponto(){
+    window.location.href="folha_ponto_user.html"
+}
